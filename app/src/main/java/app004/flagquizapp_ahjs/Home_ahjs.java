b846c92aea59348f74739183ef5928ba2ebd870e
Ariel <ajinde3033@uta.edu.ec>
2022-01-26 21:14:37 -0500

    commit Final

--- app/src/main/java/app004/flagquizapp_ahjs/Home_ahjs.java
+++ app/src/main/java/app004/flagquizapp_ahjs/Home_ahjs.java
@@ -1,15 +1,35 @@
 package app004.flagquizapp_ahjs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import app004.flagquizapp.R;
 
 public class Home_ahjs extends AppCompatActivity {
-
+private TextView datosT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_ahjs);
+        datosT = findViewById(R.id.textViewinfo);
+
+        Bundle datos = this.getIntent().getExtras();
+
+
+        String mensaje = datos.getString("nombre");
+        String nivel = datos.getString("nivel");
+        datosT.setText("Jugador : "+mensaje+"  Nivel: "+nivel+"");
+
+
+
     }
+
+    public void Jugar(View v){
+        Intent ven=new Intent(this, MainActivity_ahjs.class);
+        startActivity(ven);
+    }
+
 }
